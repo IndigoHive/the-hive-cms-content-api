142557ed4d3817625a3771eeffa4f920f32900a1
Fernando Kuteken <fskuteken@gmail.com>
2024-01-11 11:45:06 -0300

    Allow other types on unknown query param

--- src/clients/content/types/get-contents-page-query.ts
+++ src/clients/content/types/get-contents-page-query.ts
@@ -3,5 +3,5 @@ import type { Pagination } from '../../../types'
 export type GetContentsPageQuery = Pagination & {
   id?: string
   idIn?: string[]
-  [key: string]: string | string[] | undefined
+  [key: string]: string | string[] | number | number[] | Date | Date[] | boolean | boolean[] | undefined
 }
