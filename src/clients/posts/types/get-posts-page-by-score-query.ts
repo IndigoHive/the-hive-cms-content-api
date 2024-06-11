93392eee4745d0545ddb402c52b983f32804a361
YanGidorini <ygidorini@gmail.com>
2024-06-11 18:15:37 -0300

    add get

--- src/clients/posts/types/get-posts-page-by-score-query.ts
+++ src/clients/posts/types/get-posts-page-by-score-query.ts
@@ -2,5 +2,6 @@ import type { Pagination } from '../../../types'
 
 export type GetPostsPageByScoreQuery = Pagination & {
   tags?: string[]
+  excludeVersionIds?: string[]
   includeData?: boolean
 }
